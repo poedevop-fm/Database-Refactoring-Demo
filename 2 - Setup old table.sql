8f7effa9c612ef4d00db5278802031bde1b0324a
Rob Vatz <rob.vatz@sceneralabs.com>
2015-11-30 15:07:15 -0500

    Renamed files
    Renamed files to simpler text

--- 2 - Setup old table.sql
+++ 2 - Setup old table.sql
@@ -2,9 +2,12 @@
 --   1. Initial table schema and contents
 --
 USE Demo1;
-
+GO
+--
+--   Create table
+--
 IF OBJECT_ID('Customer',       'U') IS NOT NULL DROP TABLE Customer;
-
+--
 CREATE TABLE Customer
 (
    CustomerId int IDENTITY(1,1) NOT NULL PRIMARY KEY,
@@ -16,16 +19,18 @@ CREATE TABLE Customer
    BillingZIP       char(9)
 );
 GO
+--
+--   Add sample data
+--
 INSERT INTO Customer
 (Name,BillingAddress1,BillingAddress2,BillingCity,BillingState,BillingZIP)
 VALUES
 ('Fidgett, Panneck, and Runn','123 Main Street','','Fairview','XX','12345'),
 ('Dr. John H. Watson','Apt. B','221 Baker Street','Gotham','ZZ','22222'),
-('BigBusiness','456 Second Street','','Metropolis','YY','98765');
+('BigBusiness','456 Second Street','','Metropolis','YY','987654321');
 GO
 --
---
---  Test: table should have contents
+--  Verify: table should have contents
 --
 DECLARE @cnt int = (SELECT COUNT(*) FROM Customer);
 IF @cnt IS NULL OR @cnt < 3
